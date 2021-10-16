709bf837f84e11abd1c487e5f00a50d73dd205cd
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-16 08:39:27 -0300

    Encerramos a apostila 03

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+//funcional
+const App = () => {
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+    const textoDoRotulo = "Nome:"
+    const obterTextoDoBotao = () => {
+        return "Enviar"
+    }
+    const eventoBotao = () => {
+        console.log ("Botao clicado")
+    }
+    //expressão JSX
+    return (
+        <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+            <label htmlFor="nome" className="rotulo" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
+            <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+            <button style={estilosBotao} onClick={eventoBotao}>
+                {obterTextoDoBotao()}
+            </button>
+        </div>
+    )
+    
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App/>,
+    document.querySelector('#root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
